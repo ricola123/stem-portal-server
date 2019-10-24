0ead8b295ecca7017312ae085b4fdf70843081c8
Rico Wan <ricowan2007@gmail.com>
2019-10-25 06:48:22 +0800

    show user data at api/register

--- routes/api/register.js
+++ routes/api/register.js
@@ -5,6 +5,12 @@ const router = express.Router();
 
 const uri = "mongodb+srv://stem-portal-dev:stem-portal-dev@stem-portal-dx28y.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
+//Get Posts
+router.get('/', async(req, res) => {
+    const users = await loadUsersCollection();
+    res.send(await users.find({}).toArray());
+});
+
 //Register New User
 router.post('/', async(req, res) => {
     const users = await loadUsersCollection();
