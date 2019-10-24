2751854ef5027d51999c0cce382a0df920c8445b
Rico Wan <ricowan2007@gmail.com>
2019-10-25 06:45:13 +0800

    remove unnecessary files and code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// ./src/index.js
-
 // importing the dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,12 +7,6 @@ const morgan = require('morgan');
 
 // defining the Express app
 const app = express();
-
-// defining an array to work as the database (temporary solution)
-const ads = [
-  {title: 'Hello, world (again)!'},
-  {name: 'Chris Chan'}
-];
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -28,19 +20,11 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
-/*
-// defining an endpoint to return all ads
-app.get('/', (req, res) => {
-  res.send(ads);
-});
-*/
-
 const posts = require('./routes/api/posts');
 const register = require('./routes/api/register');
 
 app.use('/api/posts', posts);
 app.use('/api/register', register);
-
 
 // starting the server
 const port = process.env.PORT || 8000;
