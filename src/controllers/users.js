1afe447983fa9a3533f1c9a20e6e62a07909bdbd
alvinchristopher <alvinchristopher.susanto@gmail.com>
2019-11-01 05:34:56 +0800

    Added authentication module

---
+++ src/controllers/users.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const User = require('../models/users');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  add: (req, res) => {
+    const that = mongoose.connect(process.env.MONGO_URL, { useNewUrlParser : true }, err => {
+      let result = {};
+      let status = 201;
+      if (!err) {
+        const { username, password } = req.body;
+        const user = new User({ username, password, type: 'student' }); // document = instance of a model
+        // TODO: We can hash the password here before we insert instead of in the model
+        user.save((err, user) => {
+          if (!err) {
+            result.status = status;
+            result.result = user;
+          } else {
+            status = 500;
+            result.status = status;
+            result.error = err;
+          }
+          res.status(status).send(result);
+        });
+      } else {
+        status = 500;
+        result.status = status;
+        result.error = err;
+        res.status(status).send(result);
+      }
+    });
+  },
+  login: (req, res) => {
+    const { username, password } = req.body;
+    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, err => {
+      let result = {};
+      let status = 200;
+      if(!err) {
+        User.findOne({ username }, (err, user) => {
+          if (!err && user) {
+            // We could compare passwords in our model instead of below as well
+            bcrypt.compare(password, user.password).then(match => {
+              if (match) {
+                status = 200;
+                // Create a token
+                const payload = { user: user.username };
+                const options = { expiresIn: '7d', issuer: 'https://www.stem-portal.edu.hk' };
+                const secret = process.env.JWT_SECRET;
+                const token = jwt.sign(payload, secret, options);
+
+                result.user = user;
+                result.token = token;
+              } else {
+                status = 401;
+                result.status = status;
+                result.error = `Authentication error`;
+              }
+              res.status(status).send(result);
+            }).catch(err => {
+              status = 500;
+              result.status = status;
+              result.error = err;
+              res.status(status).send(result);
+            });
+          } else {
+            status = 404;
+            result.status = status;
+            result.error = err;
+            res.status(status).send(result);
+          }
+        });
+      } else {
+        status = 500;
+        result.status = status;
+        result.error = err;
+        res.status(status).send(result);
+      }
+    });
+  }
+}
