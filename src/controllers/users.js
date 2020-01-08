088888fddac841b0389354169c5f7f1be0aa4906
alvinchristopher <alvinchristopher.susanto@gmail.com>
2020-01-08 23:58:52 +0800

    no user duplicates

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -10,16 +10,19 @@ const utils = require('../utils')
 module.exports = {
   add: (req, res) => {
     const { username, password, email } = req.body;
-    const user = new User({ username, password, email, type: 'inactive' }); //inactive = not yet verified
-    user.save((err, user) => {
-      if (!err) {
-        const token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
-        utils.sendVerifyEmail(user, token.token);
-        res.status(201).send({ user });
-      } else {
-        res.status(500).send({ error: err });
-      }
-    });
+    User.findOne({ username }, user => {
+      if (user) return res.status(400).send({ error: 'An existing user has the same username' });
+      user = new User({ username, password, email, type: 'inactive' }); //inactive = not yet verified
+      user.save((err, user) => {
+        if (!err) {
+          const token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+          utils.sendVerifyEmail(user, token.token);
+          res.status(201).send({ user });
+        } else {
+          res.status(500).send({ error: err });
+        }
+      });
+    })
   },
   login: (req, res) => {
     const { username, password } = req.body;
@@ -45,19 +48,24 @@ module.exports = {
   },
   activate: (req, res) => {
     const { username } = req.params;
-    const { token, cancel } = req.query;
+    const { token, cancel } = req.body;
     console.log(username, token, cancel);
 
     User.findOne({ username }, (err, user) => {
-      if (err) res.status(400).send({ error: err, description: 'No such user' });
-      if (user.type !== 'inactive') res.status(400).send({ error: 'User already verified' })
-      Token.findOne({ _userId: user._id, token }, err => {
-        if (err) res.status(400).send({ error: err, description: 'No such token, may be expired' });
-        user.type = 'activated';
-        user.save((err, user) => {
-          if (err) res.status(500).send({ error: err });
-          res.status(200).send(user);
-        });
+      if (err) return res.status(400).send({ error: err, description: 'No such user' });
+      if (user.type !== 'inactive') return res.status(400).send({ error: 'User already verified' })
+      Token.findOne({ _userId: user._id, token }, (err, token) => {
+        if (err) return res.status(400).send({ error: err, description: 'No such token, may be expired' });
+        if (cancel) {
+          token.remove();
+          res.status(200).send({ status: 'cancelled', user: {} });
+        } else {
+          user.type = 'activated';
+          user.save((err, user) => {
+            if (err) res.status(500).send({ error: err });
+            res.status(200).send({ status: 'user verified', user });
+          });
+        }
       })
     });
   }
