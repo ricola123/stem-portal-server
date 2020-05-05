46b79e45fdb680a28b87826e3743de2f7fdbf624
alvinchristopher <alvinchristopher.susanto@gmail.com>
2020-05-05 23:13:24 +0800

    upload download

--- src/services/upload.js
+++ src/services/upload.js
@@ -3,24 +3,42 @@ const { ResponseError } = require('../utils');
 
 let gfs;
 require('../loaders/database').gfs()
-    .then(res => gfs = res);
+  .then(res => gfs = res);
 
 class UploadService {
 
-    async findFile (filename, user) {
-        const files = await gfs
-            .find({ filename })
-    }
+  async getFile (filename, user) {
+    const files = await gfs
+      .find({ filename })
+      .toArray()
+    if (!files.length) throw new ResponseError(404, 'unable to find requested file');
 
-    async deleteFile (filename, user) {
-        const files = await gfs
-            .find({ filename, 'metadata.uploader': user.id })
-            .toArray()
-        if (files.length !== 1) throw new ResponseError(404, 'unable to find deletable file');
-
-        const [file] = files;
-        await gfs.delete(file._id);
+    const file = files[0];
+    if (file.metadata.type === 'teaching-materials') {
+      if (!user) throw new ResponseError(401, 'unauthorized access');
+      if (user.type !== 'teacher') throw new ResponseError(403, 'forbidden');
+      // await TeacherCircleService.consumeMaterial
     }
+    return { gfs, file };
+  }
+
+  async getFileMetadatas (filenames) {
+    const files = await gfs
+      .find({ filename: { $in: filenames } })
+      .toArray()
+    if (!files.length) throw new ResponseError(404, 'unable to find the requested file');
+    return files;
+  }
+
+  async deleteFile (filename, user) {
+    const files = await gfs
+      .find({ filename, 'metadata.uploader': user.id })
+      .toArray()
+    if (!files.length) throw new ResponseError(404, 'unable to find deletable file');
+
+    const file = files[0];
+    await gfs.delete(file._id);
+  }
 }
 
 module.exports = new UploadService();
